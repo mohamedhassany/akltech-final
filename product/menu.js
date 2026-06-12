8353778683c06d9a090a9f8673fa8d35de856d2e
mohamed hassany <162311083+mohamedhassany@users.noreply.github.com>
2026-06-12 16:01:29 +0300

    Update menu.js

--- product/menu.js
+++ product/menu.js
@@ -47,7 +47,7 @@ let menuItems = [];
 
 async function fetchMenuItems() {
     try {
-        const response = await fetch('http://127.0.0.1:5000/api/menu');
+        const response = await fetch('https://mohassanyabd-akltech-api.hf.space/api/menu');
         if (!response.ok) throw new Error('Network response was not ok');
         
         menuItems = await response.json();
